d316983a17a7ba7baac8d89143cc4543940a6551
arjun-n-academics <arjun.n.academics@gmail.com>
2025-01-24 17:29:20 +0530

    Wish Page

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ document.getElementById('entryForm').addEventListener('submit', function(e) {
     const magicWord = document.getElementById('magicWord').value;
     const errorMessage = document.getElementById('error-message');
 
-    if (magicWord.toLowerCase() === 'specialword') {
-        window.location.href = 'main.html';
+    if (magicWord.toLowerCase() === 'sha') {
+        window.location.href = 'home.html';
     } else {
         errorMessage.textContent = 'Oops! That’s not the right word. Try again!';
         errorMessage.style.display = 'block';
